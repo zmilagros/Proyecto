0ad50513db0f96a6342ddc0c76dd3aea0bfc880b
Milagros Arraras <arrarasmilagros@gmail.com>
2023-07-12 02:00:27 -0300

    cambios en views, urls, search.html

--- project/apps/cliente/urls.py
+++ project/apps/cliente/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import home, crear_clientes, home, crear_cliente
+from .views import busqueda, crear_clientes, crear_cliente, home
 
 app_name = "cliente"       
 
 urlpatterns = [
     path("", home, name="home" ),
     path('crear_clientes/', crear_clientes, name="crear_clientes"),
-    path('crear/',crear_cliente, name="crear"),
+    path('crear/', crear_cliente, name="crear"),
+    path('busqueda/', busqueda, name="busqueda"),
 ]
--- project/apps/cliente/views.py
+++ project/apps/cliente/views.py
@@ -1,15 +1,17 @@
+from datetime import date
+
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
+from django.urls import is_valid_path
+
+from .forms import ClienteForm
 
 # Create your views here.
 from .models import Cliente, Pais
 
-from .forms import ClienteForm
-
-
 def home(request):
     clientes_registros = Cliente.objects.all()
-    contexto =  {"clientes": clientes_registros}
+    contexto = {"clientes": clientes_registros}
     # return render(request, "index.html", {"clientes": clientes_registros})
     return render(request, "cliente/index.html", contexto)
 
@@ -25,8 +27,8 @@ def crear_clientes(request):
     p3.save()
 
     c1 = Cliente(nombre="Maria", apellido="Arraras", nacimiento=date(1977,9,14), pais_origen_id=p1)
-    c2 = Cliente(nombre="Milagros", apellido="Arraras", nacimiento=date(2001,5,14), pais_origen_id=p2)
-    c3 = Cliente(nombre="Felipe", apellido="Polzella", nacimiento=date(1998,12,25), pais_origen_id=p3)
+    c2 = Cliente(nombre="Milagros", apellido="Arraras", nacimiento=date(2002,5,14), pais_origen_id=p2)
+    c3 = Cliente(nombre="Felipe", apellido="Polzella", nacimiento=date(2017,12,25), pais_origen_id=p3)
     c4 = Cliente(nombre="Jose", apellido="Hector", nacimiento=date(1940,10,29), pais_origen_id=p1)
 
     c1.save()
@@ -36,7 +38,7 @@ def crear_clientes(request):
     return redirect("cliente:home")
 
 def crear_cliente(request: HttpRequest) -> HttpResponse:
-    
+
     if request.method == "POST":
         form = ClienteForm(request.POST)
         if form.is_valid():
@@ -45,3 +47,21 @@ def crear_cliente(request: HttpRequest) -> HttpResponse:
     else:  # request.method == "GET"
         form = ClienteForm()
     return render(request, "cliente/crear.html", {"form": form})
+
+def busqueda(request: HttpRequest) -> HttpResponse:
+    # Búsqueda por nombre que contenga "ar"
+    cliente_nombre = Cliente.objects.filter(nombre__contains="ar")
+
+    # Nacimientos  mayores a 2000
+    cliente_nacimiento = Cliente.objects.filter(
+        nacimiento__gt=date(2000, 1, 1))
+
+    # País de origen vacío
+    cliente_pais = Cliente.objects.filter(pais_origen_id=None)
+
+    contexto = {
+        "clientes_nombre": cliente_nombre,
+        "clientes_nacimiento": cliente_nacimiento,
+        "clientes_pais": cliente_pais
+    }
+    return render(request, "cliente/search.html", contexto)
